d0f2f3e0055300ce686bb9e12f9aa0bc13a4b9cf
susiehey <suza_sabic@hotmail.com>
2025-12-03 23:01:55 +0100

    Lagt til Tailwind CSS og @tailwindcss/vite som avhengigheter, oppdatert konfigurasjoner og forbedret App-komponenten med Tailwind-stiler.

--- SportsWorld/src/App.tsx
+++ SportsWorld/src/App.tsx
@@ -1,12 +1,16 @@
 function App() {
   return (
     <>
-      <div className="text-3xl">
+      <div className="text-3xl font-bold text-center">
         SportsWorld
       </div>
-      <div className="mt-4">
+      <div className="mt-4 font-medium text-center">
         Welcome to SportsWorld!
       </div>
+
+      <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-3xl font-bold text-blue-600">Tailwind fungerer 🎉</h1>
+    </div>
     </>
   )
 }

2fab6c7df28ba3391f71d35e8f64bad736f130a2
susiehey <suza_sabic@hotmail.com>
2025-12-03 23:02:45 +0100

    Fjernet unødvendig Tailwind-stil og tekst fra App.tsx

--- SportsWorld/src/App.tsx
+++ SportsWorld/src/App.tsx
@@ -7,10 +7,6 @@ function App() {
       <div className="mt-4 font-medium text-center">
         Welcome to SportsWorld!
       </div>
-
-      <div className="min-h-screen bg-gray-100 p-8">
-      <h1 className="text-3xl font-bold text-blue-600">Tailwind fungerer 🎉</h1>
-    </div>
     </>
   )
 }

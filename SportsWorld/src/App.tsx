e935b20b05abe3039b83f204b48c1a02b5990b15
susiehey <suza_sabic@hotmail.com>
2025-12-03 23:18:08 +0100

    Endret App.tsx

--- SportsWorld/src/App.tsx
+++ SportsWorld/src/App.tsx
@@ -1,12 +1,22 @@
 function App() {
   return (
     <>
-      <div className="text-3xl font-bold text-center">
+    <header className="p-4 bg-blue-600 text-white space-y-1 text-center">
+      <div className="text-3xl font-bold">
         SportsWorld
       </div>
-      <div className="mt-4 font-medium text-center">
-        Welcome to SportsWorld!
+      <div className="font-medium">
+        Welcome to SportsWorld! 
       </div>
+      <nav className="space-x-4">
+            <a href="/" className="hover:underline">Home</a>
+            <a href="" className="hover:underline">Athletes</a>
+            <a href="" className="hover:underline">Register Athlete</a>
+            <a href="" className="hover:underline">Dashboard</a>
+        </nav>
+    </header>
+    <main></main>
+    <footer></footer>
     </>
   )
 }

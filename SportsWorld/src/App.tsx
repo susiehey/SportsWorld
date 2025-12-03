c432d6bef56ec047f66e8385b02b8d3b47d216b6
susiehey <suza_sabic@hotmail.com>
2025-12-03 22:34:43 +0100

    Lagt til enkel tekst

--- SportsWorld/src/App.tsx
+++ SportsWorld/src/App.tsx
@@ -1,7 +1,12 @@
 function App() {
   return (
     <>
-      
+      <div className="text-3xl">
+        SportsWorld
+      </div>
+      <div className="mt-4">
+        Welcome to SportsWorld!
+      </div>
     </>
   )
 }

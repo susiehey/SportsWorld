92bbb35c46da94890c9514876ef870a50f3fbeb1
susiehey <suza_sabic@hotmail.com>
2025-12-07 22:49:11 +0100

    Oppdaterte App-komponenten med forbedret navigasjon og innhold, samt la til innstillinger for live server i arbeidsområdet.

--- SportsWorld/src/App.tsx
+++ SportsWorld/src/App.tsx
@@ -1,22 +1,38 @@
 function App() {
   return (
     <>
-    <header className="p-4 bg-blue-600 text-white space-y-1 text-center">
-      <div className="text-3xl font-bold">
-        SportsWorld
-      </div>
-      <div className="font-medium">
-        Welcome to SportsWorld! 
-      </div>
-      <nav className="space-x-4">
-            <a href="/" className="hover:underline">Home</a>
-            <a href="" className="hover:underline">Athletes</a>
-            <a href="" className="hover:underline">Register Athlete</a>
-            <a href="" className="hover:underline">Dashboard</a>
-        </nav>
-    </header>
-    <main></main>
-    <footer></footer>
+      <header className="p-4 space-y-1 text-center">
+
+        {/* HEADER */}
+
+        <div className="text-4xl font-bold">
+          SportsWorld
+        </div>
+        <div className="text-md">
+          Welcome to SportsWorld! Here you can hire our MMA athletes for your events.
+        </div>
+
+        {/* NAVIGATION */}
+
+        <nav className="space-x-4">
+              <a href="/" className="hover:underline">Home</a>
+              <a href="/" className="hover:underline">Athletes</a>
+              <a href="/" className="hover:underline">Register Athlete</a>
+              <a href="/" className="hover:underline">Dashboard</a>
+          </nav>
+      </header>
+
+      <main>
+
+        {/* MAIN CONTENT */}
+
+      </main>
+
+      <footer>
+        
+        {/* FOOTER */}
+
+      </footer>
     </>
   )
 }

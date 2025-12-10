b94995a702f1c7858b613be539f0e932401ec736
susiehey <suza_sabic@hotmail.com>
2025-12-10 03:18:43 +0100

    Lagt til en del ting i API-mappen, og opprettet database

--- SportsWorldApi/Contexts/SportsWorldContext.cs
+++ SportsWorldApi/Contexts/SportsWorldContext.cs
@@ -8,4 +8,21 @@ public class SportsWorldContext(DbContextOptions<SportsWorldContext> options) : 
     public DbSet<Finance> Finances { get; set; }
     public DbSet<Athlete> Athletes { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Finance>().HasData(new Finance
+        {
+            Id = 1,
+            MoneyLeft = 10000,
+            NumberOfPurchases = 0,
+            MoneySpent = 0
+        });
+
+        modelBuilder.Entity<Athlete>().HasData(
+            new Athlete { Id = 1, Name = "Valentina Shevchenko", Gender = "Female", Price = 1000000, Image = "valentina_shevchenko.png", PurchaseStatus = false }
+        );
+    }
+
 }

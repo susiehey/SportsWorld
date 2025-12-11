d26a50ddbe69c7dd09942092788539ca346fecd4
susiehey <suza_sabic@hotmail.com>
2025-12-11 20:01:55 +0100

    RegisterPage.

--- SportsWorld/src/pages/RegisterPage.tsx
+++ SportsWorld/src/pages/RegisterPage.tsx
@@ -1,3 +1,5 @@
+import AthleteAdd from "../components/register/AthleteAdd";
+
 { /*
 Page 2. Register potential athletes
 Registering a new potential athlete. When you first register the athlete, they have status of “not purchased” as default.
@@ -10,6 +12,8 @@ export default function RegisterPage() {
                 <p className="mb-2 text-2xl font-bold">Register athletes</p>
                 <p className="text-lg font-light">Register new MMA athletes here.</p>
             </section>
+
+            <AthleteAdd></AthleteAdd>
         </>
     );
 }

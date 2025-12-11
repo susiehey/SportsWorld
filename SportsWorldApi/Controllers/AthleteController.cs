b54aead49cceb626d875ed16009b000560f97af6
susiehey <suza_sabic@hotmail.com>
2025-12-11 20:05:48 +0100

    AthleteController.

--- SportsWorldApi/Controllers/AthleteController.cs
+++ SportsWorldApi/Controllers/AthleteController.cs
@@ -19,21 +19,6 @@ public class AthleteController(SportsWorldContext _SportsWorldContext) : Control
         }
         catch
         {
-            return StatusCode(500, "Server side error when getting athletes");
-        }
-    }
-
-    [HttpPost]
-    public async Task<ActionResult> Post(Athlete newAthlete)
-    {
-        try
-        {
-            _SportsWorldContext.Athletes.Add(newAthlete);
-            await _SportsWorldContext.SaveChangesAsync();
-            return Created();
-        }
-        catch
-        {
             return StatusCode(500);
         }
     }
@@ -60,6 +45,39 @@ public class AthleteController(SportsWorldContext _SportsWorldContext) : Control
         }
     }
 
+    [HttpGet]
+    [Route("[action]/{name}")]
+    public async Task<ActionResult<List<Athlete>>> GetByName(string name)
+    {
+        try
+        {
+            List<Athlete> athletes = await _SportsWorldContext.Athletes.Where(
+                a => a.Name.Contains(name)
+            ).ToListAsync();
+
+            return Ok(athletes);
+        }
+        catch
+        {
+            return StatusCode(500);
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post(Athlete newAthlete)
+    {
+        try
+        {
+            _SportsWorldContext.Athletes.Add(newAthlete);
+            await _SportsWorldContext.SaveChangesAsync();
+            return Created();
+        }
+        catch
+        {
+            return StatusCode(500);
+        }
+    }
+
     [HttpPut]
     public async Task<IActionResult> Put(Athlete editedAthlete)
     {
@@ -67,6 +85,29 @@ public class AthleteController(SportsWorldContext _SportsWorldContext) : Control
         {
             _SportsWorldContext.Athletes.Entry(editedAthlete).State = EntityState.Modified;
             await _SportsWorldContext.SaveChangesAsync();
+            return NoContent();
+        }
+        catch
+        {
+            return StatusCode(500);
+        }
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        try
+        {
+            var athlete = await _SportsWorldContext.Athletes.FindAsync(id);
+
+            if(athlete == null)
+            {
+                return NotFound(); //404
+            }
+            
+            _SportsWorldContext.Athletes.Remove(athlete);
+            await _SportsWorldContext.SaveChangesAsync();
+
             return NoContent();
         }
         catch

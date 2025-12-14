00738050862240ecda0131c015e4e0c1b85d6b25
Emma <bordalene@gmail.com>
2025-12-14 22:04:07 +0100

    la til det som manglet og noen kommentarer i athletesService

--- SportsWorld/src/services/athletesService.ts
+++ SportsWorld/src/services/athletesService.ts
@@ -1,10 +1,16 @@
 import api from './api';
+import axios from "axios";
 import type { IAthlete } from '../interfaces/IAthlete';
 import type { IResponseList } from '../interfaces/IResponseList';
 import type { IAthleteItemResponse } from '../interfaces/IAthleteItemResponse';
 import type { IDefaultResponse } from '../interfaces/IDefaultResponse';
 
 const endpoint = "/api/athletes";
+
+interface IResponseList{
+    success: boolean,
+    data: IAthlete[] | null
+};
 
 // GET: Henter alle utøvere
 export const getAllAthletes = async () : Promise<IResponseList> => {
@@ -25,7 +31,7 @@ export const getAllAthletes = async () : Promise<IResponseList> => {
 }
 
 // GET: Henter utøver basert på navn
-export const getAthleteByName = async (name: string) : Promise<IResponseList> => {
+const getAthleteByName = async (name: string) : Promise<IResponseList> => {
     try {
         const response = await api.get(`${endpoint}/GetByName/${name}`);
         return { success: true, data: response.data
@@ -35,8 +41,13 @@ export const getAthleteByName = async (name: string) : Promise<IResponseList> =>
     }
 }
 
+interface IAthleteItemResponse{
+    success: boolean,
+    data: IAthlete | null
+}
+
 // GET: Henter utøver basert på ID
-export const getAthleteById = async (id: number) : Promise<IAthleteItemResponse> => {
+const getAthleteById = async (id: number) : Promise<IAthleteItemResponse> => {
     try {
         const response = await api.get(`${endpoint}/${id}`); 
         return { success: true, data: response.data
@@ -46,16 +57,22 @@ export const getAthleteById = async (id: number) : Promise<IAthleteItemResponse>
     }
 }
 
+interface IDefaultResponse{
+    success: boolean
+}
+
+//Emma: Det er denne som sørger for at du kan redigere informasjonen om en athlete i AthleteEdit :)
 // PUT: Oppdaterer en utøver (trenger vi denne?)
-export const putAthlete = async (editedAthlete: IAthlete) : Promise<IDefaultResponse> => {
+const putAthlete = async (editedAthlete: IAthlete) : Promise<IDefaultResponse> => {
     try {
-        const response = await api.put(endpoint, editedAthlete);
+        await api.put(endpoint, editedAthlete);
         return { success: true }
     } catch {
         return { success: false }
     }
 }
 
+//Emma: Denne tror jeg ikke vi trenger, det å registrere en ny athlete blir allerede gjort i imageuploadService og AthleteAdd
 // POST: Opretter en ny utøver
 export const createAthletes = async (input: Omit<IAthlete, "id">): Promise<IResponseList<IAthlete>> => {
     try {
@@ -72,6 +89,7 @@ export const createAthletes = async (input: Omit<IAthlete, "id">): Promise<IResp
     }
 }
 
+//Emma: Gjør denne det samme som putAthlete?
 // PUT: Oppdaterer en utøver
 export const updateAthlete = async (id: number, patch: Partial<IAthlete>): Promise<IResponseList<IAthlete>> => {
     try {
@@ -83,11 +101,16 @@ export const updateAthlete = async (id: number, patch: Partial<IAthlete>): Promi
 }
 
 // DELETE: Sletter en utøver
-export const deleteAthlete = async (id: number): Promise<void> => {
-    try {
-        await api.delete(`/athletes/${id}`);
-    } catch (error) {
-        //
+const deleteAthlete = async (id: number) : Promise<IDefaultResponse> => {
+    try{
+        await axios.delete(`${endpoint}/${id}`)
+        return {
+            success: true
+        }
+    }catch{
+        return {
+            success: false
+        }
     }
 }
 
